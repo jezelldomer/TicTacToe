291c9baade5e88ca29151e87d3c3c5d04da4d9b5
jezelldomer <jzlldomer@gmail.com>
2022-11-19 14:06:11 +0800

    adding functions for check win and for every press of the button

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,4 @@
+#tictactoe using tkinter
 from tkinter import *
 import tkinter.messagebox
 
@@ -62,3 +63,98 @@ def play():
                      command=lambda: press(9,2,2))
     button9.grid(row=2,column=2)
 
+#function for every press
+def press(num,r,c):
+    global click,count
+    if click == True:
+        labelPhoto = Label(root,image = xPhoto)
+        labelPhoto.grid(row=r,column=c)
+        if num == 1:
+            btn1.set('X')
+        elif num == 2:
+            btn2.set('X')
+        elif num == 3:
+            btn3.set('X')
+        elif num == 4:
+            btn4.set('X')
+        elif num == 5:
+            btn5.set('X')
+        elif num == 6:
+            btn6.set('X')
+        elif num == 7:
+            btn7.set('X')
+        elif num == 8:
+            btn8.set('X')
+        else:
+            btn9.set('X')
+        count += 1
+        click = False
+        checkWin()
+        
+    else:
+        labelPhoto = Label(root,image = oPhoto)
+        labelPhoto.grid(row=r,column=c)
+        if num == 1:
+            btn1.set('O')
+        elif num == 2:
+            btn2.set('O')
+        elif num == 3:
+            btn3.set('O')
+        elif num == 4:
+            btn4.set('O')
+        elif num == 5:
+            btn5.set('O')
+        elif num == 6:
+            btn6.set('O')
+        elif num == 7:
+            btn7.set('O')
+        elif num == 8:
+            btn8.set('O')
+        else:
+            btn9.set('O')
+        count += 1
+        click = True
+        checkWin()    
+            
+#function for checking who win       
+def checkWin():
+    global count,click
+    
+    if (btn1.get() == 'X' and btn2.get() == 'X' and btn3.get() == 'X' or
+        btn4.get() == 'X' and btn5.get() == 'X' and btn6.get() == 'X' or
+        btn7.get() == 'X' and btn8.get() == 'X' and btn9.get() == 'X' or
+        btn1.get() == 'X' and btn4.get() == 'X' and btn7.get() == 'X' or
+        btn2.get() == 'X' and btn5.get() == 'X' and btn8.get() == 'X' or
+        btn3.get() == 'X' and btn6.get() == 'X' and btn9.get() == 'X' or
+        btn1.get() == 'X' and btn5.get() == 'X' and btn9.get() == 'X' or
+        btn3.get() == 'X' and btn5.get() == 'X' and btn7.get() == 'X'):
+        tkinter.messagebox.showinfo("Tic-Tac-Toe", 'X Wins !')
+        click = True
+        count = 0
+        clear()
+        play()
+        
+    elif (btn1.get() == 'O' and btn2.get() == 'O' and btn3.get() == 'O' or
+          btn4.get() == 'O' and btn5.get() == 'O' and btn6.get() == 'O' or
+          btn7.get() == 'O' and btn8.get() == 'O' and btn9.get() == 'O' or
+          btn1.get() == 'O' and btn4.get() == 'O' and btn7.get() == 'O' or
+          btn2.get() == 'O' and btn5.get() == 'O' and btn8.get() == 'O' or
+          btn3.get() == 'O' and btn6.get() == 'O' and btn9.get() == 'O' or
+          btn1.get() == 'O' and btn5.get() == 'O' and btn9.get() == 'O' or
+          btn3.get() == 'O' and btn5.get() == 'O' and btn7.get() == 'O'):
+          tkinter.messagebox.showinfo("Tic-Tac-Toe", 'O Wins !')
+          count = 0
+          clear()
+          play()
+          
+    elif (count == 9):
+         tkinter.messagebox.showinfo("Tic-Tac-Toe", 'Tie Game!')
+         click = True
+         count = 0
+         clear()
+         play()
+
+
+play()
+
+root.mainloop()
